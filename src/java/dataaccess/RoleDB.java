4ac9d96d8708986fbfd509cc74eade96365ac1a9
827637 <827637@SAIT225234IT.ACDM.DS.SAIT.CA>
2021-03-21 15:35:12 -0600

    Start Lab9 Add code from Lab8 Modify each part

---
+++ src/java/dataaccess/RoleDB.java
@@ -0,0 +1,38 @@
+package dataaccess;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import models.Role;
+
+
+public class RoleDB {
+    
+     public List<Role> getAll(Role role) throws Exception {
+                
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        
+        try 
+        {
+            List<Role> roles = em.createNamedQuery("Role.findAll", Role.class).getResultList();
+            return roles;
+        }
+        finally 
+        {
+            em.close();
+        }
+
+    }
+    
+    public Role get(int roleId){
+       EntityManager em = DBUtil.getEmFactory().createEntityManager();
+       
+         try {
+            Role role = em.find(Role.class, roleId);
+            return role;
+        } finally {
+            em.close();
+            
+        }
+    }
+    
+}
